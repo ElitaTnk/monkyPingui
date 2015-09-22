00779ed231524af66d9dbbc8eac2a937c0c79180
ElitaTnk <elianalucila@gmail.com>
2015-09-22 17:37:42 -0300

    pantalla de ayuda y pantalla de menu

--- juego/ayuda.py
+++ juego/ayuda.py
@@ -2,29 +2,33 @@
 
 import pilasengine
 pilas = pilasengine.iniciar()
-texto_ayuda = """Tenes que ganar"""
 
-class PantallaAyuda(pilas.escena.Base):
+texto_ayuda = """Aca hay que poner las indicaciones de como jugar :D """
+
+class PantallaAyuda(pilasengine.escenas.Escena):
     #en esta pantalla se dan las instrucciones de juego
-    #creación de la clase para la nueva pantalla a partir de la clase madre pilas.escena.Base
+    #creación de la clase para la nueva pantalla a partir de la clase madre pilas.escena.Escena
     
-    def __init__(self):
-        pilas.escena.Base.__init__(self)
-        
     def iniciar(self):
-        #instanciamos el fondo deseado y creamos métodos para mostrar los textos de ayuda
+        #instanciamos el fondo deseado y creamos métodos para mostrar los textos de ayuda y el boton de volver
         pilas.fondos.Tarde()
         self.crear_texto_ayuda()
+        self.boton = pilas.interfaz.Boton("Volver al Inicio")
+        self.boton.y = -100
         self.boton.conectar(self.regresa_inicio)
         
     def crear_texto_ayuda(self):
-        pilas.actores.Texto("Cómo se juega?:" , y = 100)
+        pilas.actores.Texto("Como se juega?:" , y = 100)
         pilas.actores.Texto(texto_ayuda)
-        
+            
     def regresa_inicio(self):
         #importa la escena de inicio y ejecuta el cambio de pantalla
         import menu
-        pilas.cambiar_escena(escena_menu.EscenaMenu())
+        pilas.cambiar_escena(menu.EscenaMenu())
         
         
+                
+pilas.escenas.vincular(PantallaAyuda)
+pilas.escenas.PantallaAyuda() 
+          
 pilas.ejecutar()
--- juego/menu.py
+++ juego/menu.py
@@ -1,3 +1,52 @@
 # coding: utf-8
 
 import pilasengine
+pilas = pilasengine.iniciar()
+
+class EscenaMenu(pilasengine.escenas.Escena):
+    #Escena principal donde se encuentra el menu para iniciar o ver ayuda o bien salir del juego
+    #creación de la clase para la nueva pantalla a partir de la clase madre pilas.escena.Escena
+    
+    def iniciar(self):
+        #Contenido de la escena principal: logo, menu...
+        self.set_logo()
+        self.menu_inicial()
+
+    def set_logo(self):
+        #importamos logo desde carpeta
+        imagen = pilas.imagenes.cargar("data/logo.png")
+        logo = pilas.actores.Actor()
+        logo.imagen = imagen
+        logo.y = 200
+
+    def menu_inicial(self):
+        #creamos opciones y instanciamos el menu principal
+        opciones = [
+            ("Comenzar a jugar", self.comenzar_juego),
+            ("Ver ayuda", self.ayuda),
+            ("Salir", self.salir_juego)
+        ]
+        self.menu = pilas.actores.Menu(opciones, y = 0)
+
+    def comenzar_juego(self):
+        #lleva a la escena del juego en si mismo
+        import escena_juego
+        pilas.cambiar_escena(escena_juego.Juego())
+
+    def ayuda(self):
+        #lleva a la pantalla de ayuda
+        import ayuda
+        pilas.cambiar_escena(ayuda.PantallaAyuda())
+
+    def salir_juego(self):
+        #sale del juego
+        pilas.terminar()    
+     
+        
+        
+                
+pilas.escenas.vincular(EscenaMenu)
+pilas.escenas.EscenaMenu() 
+          
+pilas.ejecutar()
+
