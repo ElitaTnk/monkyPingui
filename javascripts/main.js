c2773869198744299ea445a3edbe6acc9531a1e6
ElitaTnk <elianalucila@gmail.com>
2015-10-04 21:08:48 -0300

    actualizacion de documentacion

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+    //método para stylear código
+    SyntaxHighlighter.all();
+
+    //tablas
+    $(".header-tabs li a").on("click", function (e){
+        e.preventDefault();
+        $(this).parent().siblings().removeClass("active")
+        $(this).parent().addClass("active")
+        var endpoint = $(this).attr("href");
+        $(endpoint).siblings().hide();
+        $(endpoint).show();
+    });
+});
