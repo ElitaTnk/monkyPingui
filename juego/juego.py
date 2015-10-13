7127b1e079fe5ea159608df5b0b6dc04559e7a9d
MaleCuervo <alejaxz23@gmail.ocm>
2015-10-13 00:18:10 -0300

    funcion para la figura distinta

--- juego/juego.py
+++ juego/juego.py
@@ -19,27 +19,53 @@ class Barra_tiempo(pilasengine.actores.Temporizador):
 
 class Nivel(pilasengine.escenas.Escena):
         #escena del primer nivel. Se instancia la barra de tiempo,personajes y da inicio
-       def iniciar(self):
+    def iniciar(self):
         self.pilas.fondos.Selva()
         self.tiempo = Barra_tiempo(self.pilas)      
         #se asigna un mensaje al final
         self.tiempo.ajustar(30, self.hola_mundo)
         self.tiempo.comenzar()
         self.personajes()
+        self.figura_distinta()
+    
+                            
+    def hola_mundo(self):
+        self.pilas.avisar("Fin del primer nivel") 
+           
+           
+                    
+    #crenado personajes
+    def personajes(self): 
+        self.mono = self.pilas.actores.Mono()
+        self.mono.x = -180
+        self.mono1 = self.pilas.actores.Mono() 
+        self.mono1.x = 0
+        self.figu_distinta = self.pilas.actores.Zanahoria()
+        self.figu_distinta.x = 180 
+        self.nave = self.pilas.actores.NaveRoja()
+        self.nave.y = -180 
+    
+    
+                           
+     #creando las colisiones            
+    def figura_distinta(self):
+        #self.figuras_distintas = [figu_distinta] 
+        self.figu_distinta.aprender(self.pilas.habilidades.PuedeExplotar)
+        self.figu_distinta.radio_de_colision = 100
+        self.pilas.colisiones.agregar(self.figu_distinta, self.nave, self.figura_distinta)
         
 
-       def personajes(self): 
-           self.mono = self.pilas.actores.Mono()
-           self.mono.x = -180
-           self.mono1 = self.pilas.actores.Mono() 
-           self.mono1.x = 0
-           self.zanahoria = self.pilas.actores.Zanahoria()
-           self.zanahoria.x = 180 
-           self.nave = self.pilas.actores.NaveRoja()
-           self.nave.y = -180 
-                       
-       def hola_mundo(self):
-            self.pilas.avisar("Fin del primer nivel")   
+        
+           
+           
+       
+       
+        
+                
+              
+       
+       
+       
      
   
  
