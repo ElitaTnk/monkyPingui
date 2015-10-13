c1b6e72fad1112b26c97def93b22d6c2b6f9082a
ElitaTnk <elianalucila@gmail.com>
2015-10-12 22:46:44 -0300

    nivel 1 mejoras

--- juego/juego.py
+++ juego/juego.py
@@ -2,6 +2,7 @@
 
 import pilasengine
 import pilasengine.colores
+import random
 
 
 class Barra_tiempo(pilasengine.actores.Temporizador):
@@ -14,25 +15,48 @@ class Barra_tiempo(pilasengine.actores.Temporizador):
         self.x = 260
         self.texto.y = 100
         self.texto.x = 250
-        
 
-class Nivel(pilasengine.escenas.Escena, pilasengine.actores.Mono,pilasengine.actores.Pingu,pilasengine.actores.Martian ):
+class Set_figuras():
+    #Clase que genera tres figuras con posicion variada
+    def __init__(self, pilas, posiciones = [0, -170, 170]):
+        self.posiciones = posiciones
+        self.pilas = pilas
+
+        self.figuras_distintas = [pilas.actores.Zanahoria, pilas.actores.Tortuga, pilas.actores.Aceituna]
+
+        random.shuffle(self.posiciones)
+        random.shuffle(self.figuras_distintas)
+
+        print(self.figuras_distintas[1])
+        self.figura_mono1 = self.pilas.actores.Mono(self.posiciones[0],y = 100) 
+        self.figura_mono2 = self.pilas.actores.Mono(self.posiciones[1],y = 100) 
+        self.figura_random = self.figuras_distintas[1](self.posiciones[2],y = 100) 
+
+        self.figura_mono1.escala = 0.9
+        self.figura_mono2.escala = 0.9
+        self.figura_random.escala = 1.4
+
+    def eliminar(self):
+        self.figura_mono1.eliminar()
+        self.figura_mono2.eliminar()
+        self.figura_random.eliminar()
+
+
+class Nivel(pilasengine.escenas.Escena):
     #escena del primer nivel. Se instancia la barra de tiempo y da inicio, 
     def iniciar(self):
         self.pilas.fondos.Selva()
         self.tiempo = Barra_tiempo(self.pilas)
         
         #Aparecen los personajes del primer nivel
-        self.pilas.actores.Mono(x = 0 ,y = 0) 
-        self.pilas.actores.Mono(x = -170)
-        self.pilas.actores.Pingu(x = 170, y = -50)
-        self.pilas.actores.Martian(x= 0,y = -170)
+        self.pingui = self.pilas.actores.Pingu(x = 0, y = -170)
+        self.figuras = Set_figuras(self.pilas)
         
         #se asigna un mensaje al final
         self.tiempo.ajustar(30, self.hola_mundo)
         self.tiempo.comenzar()
           
-
     def hola_mundo(self):
         self.pilas.avisar("Fin del primer nivel")
-
+        self.figuras.eliminar()
+        self.pingui.eliminar()
