b4df3c106bb3ca2dde204eeec6134347ff57dd22
ElitaTnk <elianalucila@gmail.com>
2015-10-17 14:23:30 -0300

    arreglo de colisiones, documentacion de las funciones

--- juego/juego.py
+++ juego/juego.py
@@ -3,6 +3,8 @@
 import pilasengine
 import pilasengine.colores
 import random
+from threading import Timer
+
 
 
 class Barra_tiempo(pilasengine.actores.Temporizador):
@@ -17,8 +19,10 @@ class Barra_tiempo(pilasengine.actores.Temporizador):
         self.texto.x = 250
 
 class Set_figuras():
-    #Clase que genera tres figuras con posicion variada
+    """Clase que permite generar dos figuras iguales y una variable distinta"""
+
     def __init__(self, pilas, posiciones = [0, -170, 170]):
+        """inicio de la clase, si ninguna posicion es pasada usa los valores por default"""
         self.posiciones = posiciones
         self.pilas = pilas
 
@@ -27,28 +31,42 @@ class Set_figuras():
         random.shuffle(self.posiciones)
         random.shuffle(self.figuras_distintas)
 
-        print(self.figuras_distintas[1])
-        self.figura_mono1 = self.pilas.actores.Mono(self.posiciones[0],y = 100) 
-        self.figura_mono2 = self.pilas.actores.Mono(self.posiciones[1],y = 100) 
-        self.figura_random = self.figuras_distintas[1](self.posiciones[2],y = 100) 
+        self.figura_mono1 = self.pilas.actores.Mono(self.posiciones[0],y = 80) 
+        self.figura_mono2 = self.pilas.actores.Mono(self.posiciones[1],y = 80) 
+        self.figura_random = self.figuras_distintas[1](self.posiciones[2],y = 80) 
 
         self.figura_mono1.escala = 0.9
         self.figura_mono2.escala = 0.9
         self.figura_random.escala = 1.4
 
-        self.figura_random.radio_de_colision = 100
+        self.figura_random.radio_de_colision = 50
+        self.permite_colision()
 
     def eliminar(self):
+        """Elimina todas las figuras"""
         self.figura_mono1.eliminar()
         self.figura_mono2.eliminar()
         self.figura_random.eliminar()
 
+    def permite_colision(self):
+        """Crea etiquetas para determinar opcion correcta o incorrecta"""
+        self.figura_mono1.etiquetas.agregar("opcion_erronea")
+        self.figura_mono2.etiquetas.agregar("opcion_erronea")
+        self.figura_random.etiquetas.agregar("opcion_correcta")
+
 
 class Nivel(pilasengine.escenas.Escena):
-    #escena del primer nivel. Se instancia la barra de tiempo y da inicio, 
+    """escena del primer nivel.
+        Se instancia la barra de tiempo y da inicio.
+        Se generan los personajes con sus caracteristicas modificadas para el juego.
+        Se vinculan las colisiones y las tareas""" 
+
     def iniciar(self):
         self.pilas.fondos.Selva()
+
         self.tiempo = Barra_tiempo(self.pilas)
+        self.tiempo.ajustar(30, self.termina_el_tiempo)
+        self.tiempo.comenzar()
 
         #puntaje
         self.puntaje = self.pilas.actores.Puntaje(x = 190, y = 160)
@@ -60,68 +78,77 @@ class Nivel(pilasengine.escenas.Escena):
             element.escala = 0.5
 
         #Aparecen los personajes del primer nivel
-        self.pingui = self.pilas.actores.Pingu(x = 0, y = -170)
-        self.pingui.radio_de_colision = 100
         self.figuras = Set_figuras(self.pilas)
-
-        self.figuras.figura_mono1.etiquetas.agregar("opcion_erronea")
-        self.figuras.figura_mono2.etiquetas.agregar("opcion_erronea")
-        self.figuras.figura_random.etiquetas.agregar("opcion_correcta")
+        self.pingui = self.pilas.actores.Pingu(x = 0, y = -170)
+        self.pingui.radio_de_colision = 50
+        self.pingui.centro = ("centro", "centro")
         self.pingui.etiquetas.agregar("pingui")
-
-        #se asigna un mensaje al final
-
-        self.tiempo.ajustar(30, self.hola_mundo)
-        self.tiempo.comenzar()
-
-        #pingui no se salga de la pantalla
         self.pingui.aprender(self.pilas.habilidades.LimitadoABordesDePantalla)
 
         self.pilas.colisiones.agregar("pingui", "opcion_correcta", self.acierta)
         self.pilas.colisiones.agregar("pingui", "opcion_erronea", self.erronea)
-          
-    def hola_mundo(self):
-        self.pilas.avisar("Fin del primer nivel")
-        self.figuras.eliminar()
+
+        self.pilas.tareas.condicional(3, self.refrescar_figuras)
+        self.pilas.tareas.condicional(1, self.chequear_vidas)
+
+  
+    def eliminar_set_figuras(self):
+        """elimina el set entero de figuras"""
+        if self.figuras != None:
+            self.figuras.eliminar()
+            self.figuras = None
+
+       
+    def termina_el_tiempo(self):
+        """Se ejecuta cuando el tiempo llega a 0"""
+        self.pilas.avisar("Se acabo el tiempo, Fin del primer nivel")
+        self.eliminar_set_figuras()
         self.pingui.eliminar()
 
 
     def acierta(self):
-        self.figuras.figura_random.eliminar()
-        self.puntaje.aumentar(10)
+        """Efecto de la colision correcta"""
+        if self.figuras != None:
+            self.puntaje.aumentar(10)
+            self.eliminar_set_figuras()
 
     def erronea(self):
-        self.figuras.eliminar()
-        self.vidas[0]._destruir()
+        """Efecto de la colision incorrecta"""
+        if self.figuras != None:
+            self.eliminar_set_figuras()
+            self.vidas[0]._destruir()
+            self.vidas.pop(0)
 
 
-"""
-aciertos = []
-errores = []
-figuras = []
+    def refrescar_figuras(self):
+        """Si hubo una colision y hay tiempo vuelve a generar las figuras"""
+        if self.figuras == None:
+            self.figuras = Set_figuras(self.pilas)
 
-def cuanto_toca_figura_bien(personaje, figura):
-    i.eliminar()
-    puntos.aumentar(10)
-    puntos.escala = 2
-    puntos.escala = [1], 0.2
-    puntos.rotacion = random.randint(30, 60)
-    puntos.rotacion = [0], 0.2
+        if self.tiempo.tiempo != 0 and self.chequear_vidas() == True:
+            return True
 
-#pilas.colisiones.agregar(vaca, items, cuanto_toca_item)
+    def regresa_inicio(self):
+        #vuelve la escena de inicio y ejecuta el cambio de pantalla
+        self.pilas.escenas.EscenaMenu()
 
+    def animacion_textoPerder(self, texto):
+        self.textoFin.escala = 2
+        self.textoFin.escala = [1], 1.5
 
-def crear_set():
-    un_set = Set_figuras(pilas)
-    figuras.append(un_set)
-    return True
+    def chequear_vidas(self):
+        if len(self.vidas) == 0:
+            self.textoFin = self.pilas.actores.Texto("Perdiste!!!")
+            
+            self.animacion_textoPerder(self.textoFin)
+            self.boton = self.pilas.interfaz.Boton("Volver al Inicio")
+            self.boton.y = -100
+            self.boton.conectar(self.regresa_inicio)
+            self.eliminar_set_figuras()
+            self.tiempo.detener()
+            self.pingui.eliminar()
+            self.puntaje.eliminar()
+            return False
 
-pilas.tareas.agregar(3.3, crear_set)
-
-
-def cuanto_toca_figura_mal(vaca, enemigo):
-    vaca.perder()
-    enemigo.eliminar()
-
-#pilas.colisiones.agregar(vaca, enemigos, cuanto_toca_enemigo)
-"""
+        else:
+            return True
