dd92495d18c9a7a62748df88bae0243242ac9ffd
MaleCuervo <alejaxz23@gmail.ocm>
2015-10-20 08:56:33 -0300

    arreglos para el puntaje

--- juego/juego.py
+++ juego/juego.py
@@ -31,16 +31,19 @@ class Set_figuras():
         random.shuffle(self.posiciones)
         random.shuffle(self.figuras_distintas)
 
-        self.figura_mono1 = self.pilas.actores.Mono(self.posiciones[0],y = 80) 
-        self.figura_mono2 = self.pilas.actores.Mono(self.posiciones[1],y = 80) 
+        self.figura_mono1 = self.pilas.actores.Mono(self.posiciones[0],y = 80)
+        self.figura_mono2 = self.pilas.actores.Mono(self.posiciones[1],y = 80)
         self.figura_random = self.figuras_distintas[1](self.posiciones[2],y = 80) 
-
+        
+        
+       
         self.figura_mono1.escala = 0.9
         self.figura_mono2.escala = 0.9
         self.figura_random.escala = 1.4
 
         self.figura_random.radio_de_colision = 50
         self.permite_colision()
+        
 
     def eliminar(self):
         """Elimina todas las figuras"""
@@ -50,7 +53,7 @@ class Set_figuras():
 
     def permite_colision(self):
         """Crea etiquetas para determinar opcion correcta o incorrecta"""
-        self.figura_mono1.etiquetas.agregar("opcion_erronea")
+        self.figura_mono1.etiquetas.agregar("opcion_erronea") 
         self.figura_mono2.etiquetas.agregar("opcion_erronea")
         self.figura_random.etiquetas.agregar("opcion_correcta")
 
@@ -69,7 +72,11 @@ class Nivel(pilasengine.escenas.Escena):
         self.tiempo.comenzar()
 
         #puntaje
-        self.puntaje = self.pilas.actores.Puntaje(x = 190, y = 160)
+        self.puntaje = self.pilas.actores.Puntaje(x = 10, y = 200)
+        self.texto = self.pilas.actores.Texto("Puntaje:")
+        self.texto.color = pilasengine.colores.negro
+        self.texto.y = 200
+        self.texto.x = -50
         
         #vidas
         self.vidas = [self.pilas.actores.Estrella(x = -200, y = 200), self.pilas.actores.Estrella(x = -240, y = 200), self.pilas.actores.Estrella(x = -280, y = 200)]
@@ -87,10 +94,12 @@ class Nivel(pilasengine.escenas.Escena):
 
         self.pilas.colisiones.agregar("pingui", "opcion_correcta", self.acierta)
         self.pilas.colisiones.agregar("pingui", "opcion_erronea", self.erronea)
+        
+        
 
         self.pilas.tareas.condicional(3, self.refrescar_figuras)
         self.pilas.tareas.condicional(1, self.chequear_vidas)
-
+        
   
     def eliminar_set_figuras(self):
         """elimina el set entero de figuras"""
@@ -111,12 +120,12 @@ class Nivel(pilasengine.escenas.Escena):
         if self.figuras != None:
             self.puntaje.aumentar(10)
             self.eliminar_set_figuras()
-
+#######
     def erronea(self):
         """Efecto de la colision incorrecta"""
         if self.figuras != None:
             self.eliminar_set_figuras()
-            self.vidas[0]._destruir()
+            self.vidas[0].eliminar()
             self.vidas.pop(0)
 
 
@@ -152,3 +161,7 @@ class Nivel(pilasengine.escenas.Escena):
 
         else:
             return True
+            
+            
+            
+#class nivel_2(pilasengine.escenas.Escena, Nivel):            
